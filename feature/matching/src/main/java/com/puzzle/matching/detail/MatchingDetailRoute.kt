ab3ea4ebc3661dda25db7528bb9135e93ff30fa3
sksowk156 <sksowk156@gmail.com>
2024-12-28 16:06:17 +0900

    [PC-190] conflict 수정

--- feature/matching/src/main/java/com/puzzle/matching/detail/MatchingDetailRoute.kt
+++ feature/matching/src/main/java/com/puzzle/matching/detail/MatchingDetailRoute.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
