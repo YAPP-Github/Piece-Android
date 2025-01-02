0ad231adb96bfc3de757fbb1207a048eb03058a9
tgyuuAn <atg0614@gmail.com>
2025-01-02 18:42:33 +0900

    [PC-259] ErrorHelper 추가

---
+++ core/domain/src/main/java/com/puzzle/domain/model/error/ErrorHelper.kt
@@ -0,0 +1,16 @@
+package com.puzzle.domain.model.error
+
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ErrorHelper @Inject constructor() {
+    private val _errorEvent = MutableSharedFlow<Throwable>(extraBufferCapacity = 1)
+    val errorEvent = _errorEvent.asSharedFlow()
+
+    fun sendError(error: Throwable) {
+        _errorEvent.tryEmit(error)
+    }
+}
