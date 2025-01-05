8ee6aa2d61cd732b65a72859cbf850abb2736247
tgyuuAn <atg0614@gmail.com>
2025-01-05 15:20:42 +0900

    [PC-256] 약관을 불러왔을경우, 이를 Room에 저장하도록 변경

--- core/data/src/main/java/com/puzzle/data/repository/TermsRepositoryImpl.kt
+++ core/data/src/main/java/com/puzzle/data/repository/TermsRepositoryImpl.kt
@@ -1,15 +1,30 @@
 package com.puzzle.data.repository
 
+import com.puzzle.database.model.terms.TermEntity
+import com.puzzle.database.source.term.LocalTermDataSource
 import com.puzzle.domain.repository.TermsRepository
 import com.puzzle.network.source.TermDataSource
 import javax.inject.Inject
 
 class TermsRepositoryImpl @Inject constructor(
     private val termDataSource: TermDataSource,
-    private val localTermDataSource: TermDataSource,
+    private val localTermDataSource: LocalTermDataSource,
 ) : TermsRepository {
     override suspend fun loadTerms(): Result<Unit> = runCatching {
-        val terms = termDataSource.loadTerms().getOrThrow()
-        localTermDataSource
+        val terms = termDataSource.loadTerms()
+            .getOrThrow()
+            .toDomain()
+
+        val termsEntity = terms.map {
+            TermEntity(
+                termId = it.termId,
+                title = it.title,
+                content = it.content,
+                required = it.required,
+                startDate = it.startDate.toString(),
+            )
+        }
+
+        localTermDataSource.clearAndInsertTerms(termsEntity)
     }
 }
