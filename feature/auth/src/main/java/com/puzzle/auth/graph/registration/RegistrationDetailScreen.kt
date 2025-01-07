6730a62503d23d23d633a1ea34bdd5a8404219e9
tgyuuAn <atg0614@gmail.com>
2025-01-07 14:27:43 +0900

    [PC-300] PieceTopBar에 높이 60dp 고정값 삽입

--- feature/auth/src/main/java/com/puzzle/auth/graph/registration/RegistrationDetailScreen.kt
+++ feature/auth/src/main/java/com/puzzle/auth/graph/registration/RegistrationDetailScreen.kt
@@ -1,8 +1,23 @@
 package com.puzzle.auth.graph.registration
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.puzzle.designsystem.component.PieceSubBackTopBar
 
 @Composable
-internal fun RegistrationDetailScreen() {
-
+internal fun RegistrationDetailScreen(
+    termTitle: String,
+    onBackClick: () -> Unit,
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PieceSubBackTopBar(
+            title = termTitle,
+            onBackClick = onBackClick,
+            modifier = Modifier
+        )
+    }
 }
--- feature/auth/src/main/java/com/puzzle/auth/graph/registration/RegistrationScreen.kt
+++ feature/auth/src/main/java/com/puzzle/auth/graph/registration/RegistrationScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -54,7 +53,6 @@ private fun RegistrationScreen(
         PieceSubBackTopBar(
             title = "",
             onBackClick = { navigate(NavigationEvent.NavigateUp) },
-            modifier = Modifier.height(60.dp),
         )
 
         Text(
