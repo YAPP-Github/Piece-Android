af0dddd18408f03ecc9924ddd27a159d6f30b28c
tgyuuAn <atg0614@gmail.com>
2025-01-08 13:10:09 +0900

    [PC-300] PieceWebView 설정 화면에서도 사용되는 것을 고려하여 DesignSystem으로 이동

--- feature/auth/src/main/java/com/puzzle/auth/graph/registration/RegistrationDetailScreen.kt
+++ feature/auth/src/main/java/com/puzzle/auth/graph/registration/RegistrationDetailScreen.kt
@@ -1,24 +1,16 @@
 package com.puzzle.auth.graph.registration
 
-import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import com.puzzle.designsystem.component.PieceSolidButton
 import com.puzzle.designsystem.component.PieceSubBackTopBar
+import com.puzzle.designsystem.component.PieceWebView
 import com.puzzle.domain.model.terms.Term
 
 @Composable
@@ -54,27 +46,4 @@ internal fun RegistrationDetailScreen(
                 .padding(top = 12.dp, bottom = 10.dp),
         )
     }
-}
-
-@Composable
-internal fun PieceWebView(
-    url: String,
-    modifier: Modifier = Modifier,
-) {
-    val context = LocalContext.current
-    var webView by remember { mutableStateOf<WebView?>(null) }
-
-    AndroidView(
-        factory = {
-            webView = WebView(context).apply {
-                settings.javaScriptEnabled = true
-                webViewClient = object : WebViewClient() {}
-                webChromeClient = object : WebChromeClient() {}
-            }
-            webView!!
-        },
-        update = { it.loadUrl(url) },
-        onRelease = { webView?.destroy() },
-        modifier = modifier,
-    )
 }
