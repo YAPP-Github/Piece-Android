d87cb52cae3d672154217c748a17cf3788128d59
tgyuuAn <atg0614@gmail.com>
2025-01-14 18:38:23 +0900

    [PC-255] LoginButton Throttle 추가

--- core/designsystem/src/main/java/com/puzzle/designsystem/component/Button.kt
+++ core/designsystem/src/main/java/com/puzzle/designsystem/component/Button.kt
@@ -16,6 +16,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -162,8 +166,16 @@ fun PieceLoginButton(
     containerColor: Color = PieceTheme.colors.primaryDefault,
     labelColor: Color = PieceTheme.colors.black
 ) {
+    var lastClickTime by remember { mutableLongStateOf(0L) }
+
     Button(
-        onClick = onClick,
+        onClick = {
+            val currentTime = System.currentTimeMillis()
+            if (currentTime - lastClickTime >= 2000L) {
+                onClick()
+                lastClickTime = currentTime
+            }
+        },
         enabled = enabled,
         shape = RoundedCornerShape(8.dp),
         border = border,
