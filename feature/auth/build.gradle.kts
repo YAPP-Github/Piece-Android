ebc49706e148c778987ade0fdea9dc4c83f8d961
tgyuuAn <atg0614@gmail.com>
2025-01-14 16:50:09 +0900

    [PC-255] GoogleLogin, Credentials 의존성 추가

--- feature/auth/build.gradle.kts
+++ feature/auth/build.gradle.kts
@@ -8,6 +8,10 @@ android {
 
 dependencies {
     implementation(projects.core.common)
+
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
     implementation(libs.kakao.user)
     implementation(libs.accompanist.permission)
 }
