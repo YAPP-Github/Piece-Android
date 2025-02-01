bdec8fd4c3dc261acb5a137045b1309df93c9df3
tgyuuAn <atg0614@gmail.com>
2025-02-01 19:18:06 +0900

    [PC-476] ValuePickEntity 추가

---
+++ core/database/src/main/java/com/puzzle/database/model/matching/ValuePickEntity.kt
@@ -0,0 +1,58 @@
+package com.puzzle.database.model.matching
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+import com.puzzle.domain.model.matching.Answer
+import com.puzzle.domain.model.matching.ValuePick
+
+@Entity(tableName = "value_pick")
+data class ValuePickEntity(
+    @Embedded val valuePickQuestion: ValuePickQuestion,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "questionsId",
+        entity = ValuePickAnswer::class,
+    )
+    val answers: List<ValuePickAnswer>,
+) {
+    fun toDomain() = ValuePick(
+        id = valuePickQuestion.id,
+        category = valuePickQuestion.category,
+        question = valuePickQuestion.question,
+        answers = answers.map {
+            Answer(
+                number = it.number,
+                content = it.content,
+            )
+        },
+        isSimilarToMe = false,
+    )
+}
+
+@Entity(tableName = "value_pick_question")
+data class ValuePickQuestion(
+    @PrimaryKey val id: Int,
+    val category: String,
+    val question: String,
+)
+
+@Entity(
+    tableName = "value_pick_answer",
+    foreignKeys = [
+        ForeignKey(
+            entity = ValuePickQuestion::class,
+            parentColumns = ["id"],
+            childColumns = ["questionsId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class ValuePickAnswer(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val questionsId: Int,
+    val number: Int,
+    val content: String,
+)
