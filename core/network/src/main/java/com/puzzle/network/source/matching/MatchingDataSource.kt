87ba42951c95490c9267a35d4e6f0cfae77f46b0
tgyuuAn <atg0614@gmail.com>
2025-02-11 19:26:24 +0900

    [PC-555] MatchingDataSource Interface, 구현체 분리

--- core/network/src/main/java/com/puzzle/network/source/matching/MatchingDataSource.kt
+++ core/network/src/main/java/com/puzzle/network/source/matching/MatchingDataSource.kt
@@ -1,42 +1,20 @@
 package com.puzzle.network.source.matching
 
-import com.puzzle.network.api.PieceApi
-import com.puzzle.network.model.matching.BlockContactsRequest
 import com.puzzle.network.model.matching.GetMatchInfoResponse
 import com.puzzle.network.model.matching.GetOpponentProfileBasicResponse
 import com.puzzle.network.model.matching.GetOpponentProfileImageResponse
 import com.puzzle.network.model.matching.GetOpponentValuePicksResponse
 import com.puzzle.network.model.matching.GetOpponentValueTalksResponse
-import com.puzzle.network.model.matching.ReportUserRequest
-import com.puzzle.network.model.unwrapData
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class MatchingDataSource @Inject constructor(
-    private val pieceApi: PieceApi
-) {
-    suspend fun reportUser(userId: Int, reason: String): Result<Unit> =
-        pieceApi.reportUser(ReportUserRequest(userId = userId, reason = reason)).unwrapData()
-
-    suspend fun blockUser(userId: Int): Result<Unit> = pieceApi.blockUser(userId).unwrapData()
-    suspend fun blockContacts(phoneNumbers: List<String>): Result<Unit> =
-        pieceApi.blockContacts(BlockContactsRequest(phoneNumbers)).unwrapData()
-
-    suspend fun getMatchInfo(): Result<GetMatchInfoResponse> = pieceApi.getMatchInfo().unwrapData()
-    suspend fun getOpponentValueTalks(): Result<GetOpponentValueTalksResponse> =
-        pieceApi.getOpponentValueTalks().unwrapData()
-
-    suspend fun getOpponentValuePicks(): Result<GetOpponentValuePicksResponse> =
-        pieceApi.getOpponentValuePicks().unwrapData()
-
-    suspend fun getOpponentProfileBasic(): Result<GetOpponentProfileBasicResponse> =
-        pieceApi.getOpponentProfileBasic().unwrapData()
-
-    suspend fun getOpponentProfileImage(): Result<GetOpponentProfileImageResponse> =
-        pieceApi.getOpponentProfileImage().unwrapData()
-
-    suspend fun checkMatchingPiece(): Result<Unit> = pieceApi.checkMatchingPiece().unwrapData()
-    suspend fun acceptMatching(): Result<Unit> = pieceApi.acceptMatching().unwrapData()
-
+interface MatchingDataSource {
+    suspend fun reportUser(userId: Int, reason: String): Result<Unit>
+    suspend fun blockUser(userId: Int): Result<Unit>
+    suspend fun blockContacts(phoneNumbers: List<String>): Result<Unit>
+    suspend fun getMatchInfo(): Result<GetMatchInfoResponse>
+    suspend fun getOpponentValueTalks(): Result<GetOpponentValueTalksResponse>
+    suspend fun getOpponentValuePicks(): Result<GetOpponentValuePicksResponse>
+    suspend fun getOpponentProfileBasic(): Result<GetOpponentProfileBasicResponse>
+    suspend fun getOpponentProfileImage(): Result<GetOpponentProfileImageResponse>
+    suspend fun checkMatchingPiece(): Result<Unit>
+    suspend fun acceptMatching(): Result<Unit>
 }
