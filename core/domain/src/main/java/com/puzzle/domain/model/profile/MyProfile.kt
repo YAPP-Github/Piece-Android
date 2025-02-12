80dcad6400cb2a5534f8b75d4eb5ef8cb017f4db
tgyuuAn <atg0614@gmail.com>
2025-02-12 19:35:56 +0900

    [PC-565] 내 프로필 진짜 데이터로 변경

--- core/domain/src/main/java/com/puzzle/domain/model/profile/MyProfile.kt
+++ core/domain/src/main/java/com/puzzle/domain/model/profile/MyProfile.kt
@@ -1,21 +1,5 @@
 package com.puzzle.domain.model.profile
 
-data class MyProfile(
-    val description: String,
-    val nickname: String,
-    val age: Int,
-    val birthDate: String,
-    val height: Int,
-    val weight: Int,
-    val location: String,
-    val job: String,
-    val smokingStatus: String,
-    val imageUrl: String,
-    val contacts: List<Contact>,
-    val valuePicks: List<MyValuePick>,
-    val valueTalks: List<MyValueTalk>,
-)
-
 data class MyProfileBasic(
     val description: String,
     val nickname: String,
