f6ffd32ab3e858b55e1d2ae1d2449b58e645ceee
tgyuuAn <atg0614@gmail.com>
2025-03-02 19:56:42 +0900

    [PC-685] Analytics 모듈 추가

---
+++ core/analytics/build.gradle.kts
@@ -0,0 +1,27 @@
+import java.util.Properties
+
+plugins {
+    id("piece.android.library")
+}
+
+android {
+    namespace = "com.puzzle.analytics"
+
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").bufferedReader())
+        buildConfigField(
+            "String",
+            "AMPLITUDE_API_KEY",
+            "\"${properties["AMPLITUDE_API_KEY"]}\"",
+        )
+    }
+
+    buildFeatures{
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.amplitude.analytics)
+}
